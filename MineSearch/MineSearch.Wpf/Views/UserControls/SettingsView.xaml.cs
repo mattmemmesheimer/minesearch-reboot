c995faa8fd39605727cb3a2c2c48c0fae4137edc
mattmemmesheimer <m.memmesheimer@gmail.com>
2016-02-17 00:28:48 -0600

    Adding crude radio buttons in view.

--- MineSearch/MineSearch.Wpf/Views/UserControls/SettingsView.xaml.cs
+++ MineSearch/MineSearch.Wpf/Views/UserControls/SettingsView.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace MineSearch.Wpf.Views.UserControls
+﻿using MineSearch.Wpf.ViewModels;
+
+namespace MineSearch.Wpf.Views.UserControls
 {
     /// <summary>
     /// Interaction logic for SettingsView.xaml
     /// </summary>
     public partial class SettingsView
     {
+        public SettingsViewModel ViewModel { get { return DataContext as SettingsViewModel; } }
+
         public SettingsView()
         {
             InitializeComponent();
